7ea11858349ade69149a0ea4cf853468bf0a3af9
xlimitan <xlimitan@yandex.ru>
2023-04-23 11:30:23 +0300

    2

---
+++ src/main/java/com/calculator/calculator/CalculatorService.java
@@ -0,0 +1,8 @@
+package com.calculator.calculator;
+
+public interface CalculatorService {
+    int plus (int a, int b);
+    int minus (int a, int b);
+    int multiply (int a, int b);
+    int divide (int a, int b);
+}
